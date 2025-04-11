f9ba039acd3674fc214124c4a3e66288137b146d
crL96 <190506813+crL96@users.noreply.github.com>
2025-04-11 10:31:59 +0200

    Add quantity and add to cart button

--- src/components/itemCard/ItemCard.jsx
+++ src/components/itemCard/ItemCard.jsx
@@ -1,13 +1,25 @@
 import style from "./itemCard.module.css";
+import { useState } from "react";
 
-function ItemCard({data}) {
+function ItemCard({ data, handleAddToCart }) {
+    const [itemQuantity, setItemQuantity] = useState(1);
+
     return (
         <div className={style.itemCard}>
             <h2>{data.title}</h2>
             <img src={data.image} alt="" />
             <p>${data.price}</p>
+            <form className={style.addToCartForm}>
+                <input
+                    type="number"
+                    value={itemQuantity}
+                    onChange={(event) => setItemQuantity(event.target.value)}
+                    min="1"
+                />
+                <button onClick={handleAddToCart}>Add to cart</button>
+            </form>
         </div>
-    );   
+    );
 }
 
 export default ItemCard;
