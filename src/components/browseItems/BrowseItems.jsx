1d4b33d1ead8a892bba9f45ea1ede21ce8c3fef8
crL96 <190506813+crL96@users.noreply.github.com>
2025-04-11 12:12:27 +0200

    Pass through addToCart function and run prettier

--- src/components/browseItems/BrowseItems.jsx
+++ src/components/browseItems/BrowseItems.jsx
@@ -2,22 +2,24 @@ import { useState, useEffect } from "react";
 import ItemCard from "../itemCard/ItemCard";
 import styles from "./browseItems.module.css";
 
-function BrowseItems() {
+function BrowseItems({ addToCart }) {
     const [data, setData] = useState(null);
 
     useEffect(() => {
-        fetch('https://fakestoreapi.com/products/')
-            .then(response => response.json())
-            .then(responseData => setData(responseData.slice(0, 20)));
-            // slice array incase API returns more than 20 items
+        fetch("https://fakestoreapi.com/products/")
+            .then((response) => response.json())
+            .then((responseData) => setData(responseData.slice(0, 20)));
+        // slice array incase API returns more than 20 items
     }, []);
 
-    if (data === null) return <p>Loading...</p>
+    if (data === null) return <p>Loading...</p>;
 
     return (
         <div className={styles.allItems}>
             {data.map((item) => {
-                return <ItemCard data={item} key={item.id}/>
+                return (
+                    <ItemCard data={item} key={item.id} addToCart={addToCart} />
+                );
             })}
         </div>
     );
