afe1b6887dd50fee548c29a1bd940674b5d62cc6
crL96 <190506813+crL96@users.noreply.github.com>
2025-04-11 17:15:44 +0200

    Add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import './App.css'
+import { Link } from 'react-router';
 
 function App() {
+  return (
+    <div>
+      <h1>Store4</h1>
+      <Link to="shop">Go To Store!</Link>
+    </div>
+  );
 }
 
 export default App
