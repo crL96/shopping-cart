32c83d487f9d0b3e1705ce12c10fbaa87f7d3501
crL96 <190506813+crL96@users.noreply.github.com>
2025-04-17 11:48:10 +0200

    Refactor react router to allow data to prepare for data to be passed from App to components via router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,66 @@
 import './App.css'
 import { Link } from 'react-router';
 import homePageImg from "./assets/images/homePageImg.png";
+import { createBrowserRouter, Router, RouterProvider} from "react-router-dom";
+import Shop from './components/shop/Shop.jsx';
+import CheckOutPage from './components/checkOutPage/CheckOutPage.jsx';
+import ErrorPage from './components/errorPage/ErrorPage.jsx';
+import NavBar from "./components/navBar/NavBar.jsx";
+import Cart from './components/cart/Cart.jsx';
 
-function App() {
+
+
+function HomePage() {
   return (
     <div className='homePage'>
       <img src={homePageImg} alt="Store4" />
       <Link to="shop">Go To Store!</Link>
+    </div>
+  );
+}
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: 
+      <>
+        <NavBar />
+        <HomePage />
+      </>,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: "shop",
+      element: 
+      <>
+        <NavBar />
+        <Shop />
+      </>,
+    },
+    {
+      path: "checkout",
+      element: 
+      <>
+        <NavBar />
+        <CheckOutPage />
+      </>,
+    },
+    {
+      path: "cart",
+      element: 
+      <>
+        <NavBar />
+        <Cart />
+      </>
+    },
+  ]);
+
+
+
+  return (
+    <div className='app'>
+      <RouterProvider router={router} />
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, Router, RouterProvider } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
-import Shop from './components/shop/Shop.jsx';
-import CheckOutPage from './components/checkOutPage/CheckOutPage.jsx';
-import ErrorPage from './components/errorPage/ErrorPage.jsx';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "shop",
-    element: <Shop />,
-  },
-  {
-    path: "checkout",
-    element: <CheckOutPage />,
-  },
-]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>,
 )
